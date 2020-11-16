45c73d920978aa13d8052fd32658c9125b14258d
ashwani8418 <68030476+ashwani8418@users.noreply.github.com>
2020-11-16 17:26:44 +0530

    upsdated

--- src/components/App.js
+++ src/components/App.js
@@ -1,28 +1,31 @@
-import React, { Component, useState } from "react";
+import React, {Component, useState} from "react";
 import '../styles/App.css';
 
 class App extends Component {
     constructor(props) {
-        super(props);
-        this.state = {
-            isParaShown: false,
-        };
+		super(props);
+		this.state = {
+			isParaShown: false,
+		};
+		this.showPara = this.showPara.bind(this);
+	};
 
-        this.showPara = this.showPara.bind(this);
-    }
-    showPara() {
-        this.setState({ isParaShown: true });
-    };
+	showPara() {
+		this.setState({isParaShown: true});
+	};
 
     render() {
-        return ( 
-        <div id = "main" >
-            <button id = "click" onClick ={this.showPara}> Show Paragraph </button>
-            {this.state.isParaShown ?(<p id = "para" > Hello, I 've learnt to use the full-stack evaluation tool. This makes me so happy</p>) : null
-            }
-        </div>
-
-        );
+    	return(
+    		<div id="main">
+				<button id="click" onClick={this.showPara} >Show Paragraph</button>
+				{this.state.isParaShown ? (
+				<p id="para">
+					Hello, I've learnt to use the full-stack evaluation tool. 
+					This makes me so happy
+					</p>
+				): null}
+    		</div>
+    	);
     }
 }
 
